d1d047de0835d2fa841c5ee8b4662a4940ae4192
MVStud <W0850616@myscc.ca>
2025-01-20 17:11:27 -0500

    Added some terraform code

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,31 +1,32 @@
-resource "aws_lambda_function" "myfunc' {
-    filename = data.archive_file.zip.output_path       
-    source_code_hash = data.archive_file.zip.output_base64sha256
-    fuction_name = "myfunc"
-    role = aws_iam_role.iam4lambda.arn
-    handler = "func.handler"
-    runtime = "python3.8"
+resource "aws_lambda_function" "myfunc" {
+  filename         = data.archive_file.zip.output_path       
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  function_name    = "myfunc"
+  role             = aws_iam_role.iam4lambda.arn
+  handler          = "func.handler"
+  runtime          = "python3.8"
 }
 
 resource "aws_iam_role" "iam4lambda" {
-    name = "iam4lambda"
-    assume_role_policy = <<EOF
-
+  name = "iam4lambda"
+  assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
 data "archive_file" "zip" {
-    type = "zip"
-    source_dir = "lambda"
-    output_path = "lambda.zip"
+  type        = "zip"
+  source_dir  = "lambda"
+  output_path = "lambda.zip"
 }
