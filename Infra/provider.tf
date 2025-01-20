d30b2484b4447e62ef386ba2e3c28adf83053e94
MVStud <W0850616@myscc.ca>
2025-01-20 17:14:27 -0500

    "But there are lots of errors"

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -8,7 +8,5 @@ terraform {
 }
 provider "aws" {
     profile ="my-profile"
-#    access_key = "AKIAV6L5VK5PEUVWOL5C"
- #   Secret_key = "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"
+
     region = "us-east-1"
-}
